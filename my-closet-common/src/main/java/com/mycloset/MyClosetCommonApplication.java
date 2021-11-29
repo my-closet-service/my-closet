b2bf294d040f656adfbee23034bb78485e1d5f87
JEONGHYUNJUNE <gtyb08@naver.com>
2021-11-29 20:33:15 +0900

    create project

---
+++ my-closet-common/src/main/java/com/mycloset/MyClosetCommonApplication.java
@@ -0,0 +1,13 @@
+package com.mycloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClosetCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyClosetCommonApplication.class, args);
+	}
+
+}
