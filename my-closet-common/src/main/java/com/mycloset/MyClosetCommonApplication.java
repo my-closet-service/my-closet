fe42559b03c2e72737414a665003e9b3fdc6f109
namookk <papakang22@naver.com>
2021-12-01 15:49:01 +0900

    test : h2 database connection test

--- my-closet-common/src/main/java/com/mycloset/MyClosetCommonApplication.java
+++ my-closet-common/src/main/java/com/mycloset/MyClosetCommonApplication.java
@@ -1,9 +1,16 @@
 package com.mycloset;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@EntityScan(basePackages = {"com.mycloset"})
+@Slf4j
 public class MyClosetCommonApplication {
 
 	public static void main(String[] args) {
